17e0bf778f048d277f33f71354904cf1f4d4ba58
Osiessea <osiesseasidehaven@gmail.com>
2026-03-03 20:45:03 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,19 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAgkI6wmH6sitqXMhmQ5bxcJYKRCGqhkms",
-    authDomain: "osies-seaside-haven.firebaseapp.com",
-    projectId: "osies-seaside-haven",
-    storageBucket: "osies-seaside-haven.firebasestorage.app",
-    messagingSenderId: "516451768058",
-    appId: "1:516451768058:web:8fc363ca39427cbe6c5b43"
-  };
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAgkI6wmH6sitqXMhmQ5bxcJYKRCGqhkms",
+  authDomain: "osies-seaside-haven.firebaseapp.com",
+  projectId: "osies-seaside-haven",
+  storageBucket: "osies-seaside-haven.firebasestorage.app",
+  messagingSenderId: "516451768058",
+  appId: "1:516451768058:web:8fc363ca39427cbe6c5b43"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
